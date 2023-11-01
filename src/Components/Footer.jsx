d8892f3a2ee8aa5e97970221ad371439eecfb9cf
khrise Franchesca <khrisefzm@gmail.com>
2023-11-01 10:09:52 -0500

    footer almost done only need links for social media

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,23 +1,50 @@
 import React from "react";
+import "../styles/footer.css";
 import { Link } from "react-router-dom";
+
+import fb from "../img/facebook.png";
+import ig from "../img/instagram.png";
 
 export const Footer = () => {
   let year = new Date().getFullYear();
+
   return (
-    <footer>
+    <footer className="bg-dark p-3 mt-4">
       <div>
-        <p className="text-center">Siguenes en nuestras redes sociales:</p>
+        <p className="text-center fw-bold text-light fs-5">
+          Síguenos en nuestras redes sociales:
+        </p>
         <div className="d-flex justify-content-center">
-          <Link to="" target="_blank" rel="noopener noreferrer">
-            <img src="" alt="" />
+          <Link
+            to=""
+            className="mx-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="socialMediaLogo"
+              src={fb}
+              alt="facebook"
+              loading="lazy"
+            />
           </Link>
-          <Link to="" target="_blank" rel="noopener noreferrer">
-            <img src="" alt="" />
+          <Link
+            className="mx-2"
+            to=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="socialMediaLogo"
+              src={ig}
+              alt="instagram"
+              loading="lazy"
+            />
           </Link>
         </div>
       </div>
-      <div>
-        <p className="text-center">
+      <div className="mt-3">
+        <p className="text-center text-light reduceFontSize">
           © {year} Cursos Variados. All rights reserved.
         </p>
       </div>
