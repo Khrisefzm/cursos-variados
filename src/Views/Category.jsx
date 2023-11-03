d660b243758443da92aaf8eadea5c556bfa6af03
khrise Franchesca <khrisefzm@gmail.com>
2023-11-03 12:40:03 -0500

    filter products by category in Category.jsx

--- src/Views/Category.jsx
+++ src/Views/Category.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+import products from "../mocks/products.json";
+
 export const Category = () => {
   const params = useParams();
+  const categoryId = params.id * 1;
+
+  const categoryProducts = products.filter((element) => {
+    for (let i = 0; i < element.category.length; i++) {
+      if (element.category[i] === categoryId) return true;
+    }
+    return false;
+  });
+  console.log(categoryProducts);
   return (
     <>
       <h1>Categoria {params.id}</h1>
